c3ac88158e6c296b793052f344abd1e1db0e565c
정윤환 <109188479+Torychu@users.noreply.github.com>
2023-03-16 14:21:19 +0900

    Add files via upload

---
+++ exam01.java
@@ -0,0 +1,17 @@
+package s0316;
+
+public class exam01 {
+
+	public static void main(String[] args) {
+//		int a = 2147483647;
+//		int b = 1;
+//		int c = a+b;
+//		System.out.println(c);
+//		System.out.println(b);
+		
+		int a = 128;
+		byte b = (byte) a;
+		System.out.println(b);
+	}
+
+}
---
+++ exam03.java
@@ -0,0 +1,15 @@
+package s0316;
+
+public class exam03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[10];
+		int b[] = new int[3];
+		int [] c = new int[3];
+		int d[] = {1,2,3};
+		int e[] = new int[] {1,2,3};
+
+	}
+
+}
