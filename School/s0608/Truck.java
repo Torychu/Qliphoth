7b5d492af80a5e6aee40baae93738553009b7d07
정윤환 <109188479+Torychu@users.noreply.github.com>
2023-06-08 14:01:19 +0900

    Add files via upload

---
+++ School/s0608/Truck.java
@@ -0,0 +1,13 @@
+package s0608;
+
+public class Truck extends Car{
+	int ton;
+	void speedUp() {
+		velocity += 10;
+		System.out.println("트럭속도 " + velocity + "증가");
+	}
+	
+	void speedUpUP() {
+		super.speedUp();
+	}
+}
