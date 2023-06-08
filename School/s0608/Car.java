9a5b93b5e39aa6fb5a4e9b247953da23a42f68c8
정윤환 <109188479+Torychu@users.noreply.github.com>
2023-06-08 15:21:52 +0900

    Add files via upload

--- School/s0608/Car.java
+++ School/s0608/Car.java
@@ -1,15 +1,21 @@
 package s0608;
 
 public class Car {
-	String carname;
-	String color;
-	int velocity;
-	void speedUp() {
-		velocity += 5;
-		System.out.println("속도 "+ velocity + "증가");
-	}
-	void speedDown() {
-		velocity -= 5;
-		System.out.println("속도 " + velocity + "감소");
+//	String carname;
+//	String color;
+//	int velocity;
+//	void speedUp() {
+//		velocity += 5;
+//		System.out.println("속도 "+ velocity + "증가");
+//	}
+//	void speedDown() {
+//		velocity -= 5;
+//		System.out.println("속도 " + velocity + "감소");
+//	}
+	
+	String name = "차";
+	
+	String print() {
+		return "[차의 종류는 " + name + "이다. ]";
 	}
 }
--- School/s0608/Truck.java
+++ School/s0608/Truck.java
@@ -1,13 +1,19 @@
 package s0608;
 
 public class Truck extends Car{
-	int ton;
-	void speedUp() {
-		velocity += 10;
-		System.out.println("트럭속도 " + velocity + "증가");
-	}
+//	int ton;
+//	void speedUp() {
+//		velocity += 10;
+//		System.out.println("트럭속도 " + velocity + "증가");
+//	}
+//	
+//	void speedUpUP() {
+//		super.speedUp();
+//	}
 	
-	void speedUpUP() {
-		super.speedUp();
+	String name = "트럭";
+	
+	String print() {
+		return "[차의 종류는 " + name + "이다. ]";
 	}
 }
